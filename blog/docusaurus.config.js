e5f0943db5ef22b2ac900c66587695667cb7ff83
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2025-10-06 11:03:58 -0600

    Merge pull request #330 from rvanasa/ryan/dev-env
    feat: add `npm run mops` command and set up formatter for VS Code

--- blog/docusaurus.config.js
+++ blog/docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
 						items: [
 							{
 								label: 'GitHub',
-								href: 'https://github.com/ZenVoich/mops',
+								href: 'https://github.com/dfinity/mops',
 							},
 							{
 								label: 'Twitter',
